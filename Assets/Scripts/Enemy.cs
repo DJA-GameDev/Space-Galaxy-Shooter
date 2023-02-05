80683035f2052b1b5ca50c8339494173c2061a56
Derek Anderson <derekanderson.business@gmail.com>
2023-02-05 11:35:14 -0700

    Created space shooter game with player, laser, and enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Enemy : MonoBehaviour
+{ 
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -7f)
+        {
+            float randomX = (Random.Range(-8.0f, 8.0f));
+            transform.position = new Vector3(randomX, 8.0f, 0);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        { 
+            //damage player
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Laser")
+        { 
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
